66ebf87d2fa3f29f8475bd97a5cd4beee2021086
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 12:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ function logWhisper(string) {
   console.log(whisper('hello'))
 }
 function sayHiToGrandma(string) {
-  return ("I can't hear you!")
+  return ("I can't hear you.")
 }
 function sayHiToGrandma(string){
   return ('YES INDEED!')
   expect(shout('YES INDEED!')).toEqual("I love you, too!")
 }
+function sayHiToGrandma(string) {
+  return 
