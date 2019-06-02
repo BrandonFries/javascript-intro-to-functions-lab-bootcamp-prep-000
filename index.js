8a3806edd763799480de7511cb557b88ea8bf38e
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 12:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper('hello'))
 }
+function sayHiToGrandma(string){
+  return string.toUpperCase
+}
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
