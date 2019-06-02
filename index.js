71e502407084880d3fb677ffc475d5618c9e4027
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 11:52:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(shout('HELLO'))
+}
+function logWhisper(string) {
+  console.log(whisper('hello'))
+}
+function sayHiToGrandma(string) {
+  return ("I can't hear you!")
+}
+function sayHiToGrandma(string){
+  return ('yes indeed')
+} 
