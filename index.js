d7d5fd30b79e63a45ab7738d0290c20c96773cdc
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 13:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,4 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   it('returns "I can\'t hear you!" if `string` is lowercase', function()
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-}
+}  
